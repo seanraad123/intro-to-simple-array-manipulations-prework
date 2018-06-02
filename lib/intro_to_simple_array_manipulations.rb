18e8d26fa9265a0bba1c8145dad48f13dbeed90f
Sean Conrad <seanconrad123@gmail.com>
2018-06-02 04:03:00 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -30,6 +30,10 @@ def using_concat(array1, array2)
   array1.concat(array2)
 end
 
+
 def using_insert(array, new_element)
   array.insert(3, new_element)
 end
+
+array ["hello", "sean", "michael"]
+print using_insert()
